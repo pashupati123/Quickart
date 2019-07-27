1aeb64a761f1f24b3d31935a908dd998cfdfcda6
Pashupati Nath Verma <verma2471995@gmail.com>
2019-07-27 15:31:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function openshopNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeshopNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+function openpayNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeshopNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
